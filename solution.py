45b0a7561d9130dc34e3282a21d0c3e4ecd7731c
Marcin Zatorski <mar3211233@gmail.com>
2021-03-18 01:22:26 +0100

    Add checks for validity and scoring to solution

--- solution.py
+++ solution.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+import itertools
 
 class Solution:
     cycles: Tuple[List[int], List[int]]
@@ -8,13 +9,39 @@ class Solution:
     def __init__(self, cycles: Tuple[List[int], List[int]], instance: List[List[int]]):
         self.cycles = cycles
         self.lengths = [0, 0]
-        self.total_length = self.score()
+        self.total_length = 0
+        self.check_validity(instance)
+        self.score(instance)
 
-    def score(self) -> int:
-        return 0
+    def score(self, instance: List[List[int]]) -> int:
+        for i, cycle in enumerate(self.cycles):
+            n = len(cycle)
+            length = 0
+            for v1, v2 in zip(cycle[:n - 1], cycle[1:]):
+                length += instance[v1][v2]
+            length += instance[cycle[0]][cycle[n - 1]]
+            self.lengths[i] = length
+        self.total_length = sum(self.lengths)
 
-    def is_valid(self) -> bool:
+    def check_validity(self, instance: List[List[int]]) -> bool:
+        n = len(instance)
+        cycles = self.cycles
+        len1 = len(cycles[0])
+        len2 = len(cycles[1])
+
+        if abs(len1 - len2) > 1:
+            raise ValueError('Both paths must contain half or half - 1 of vertices')
+        if len1 + len2 != n:
+            raise ValueError('Paths must contain every vertex exactly once')
+        
+        is_present = [False for i in range(n)]
+        for v in itertools.chain(cycles[0], cycles[1]):
+            if v >= n or v < 0:
+                raise ValueError('Vertex index must be between 0 and n - 1')
+            if is_present[v]:
+                raise ValueError('Paths must contain every vertex exactly once')
+            is_present[v] = True
         return True
-    
+
     def __str__(self):
         return f'Solution(cycles={self.cycles}, lengths={self.lengths}, total_length={self.total_length})'
