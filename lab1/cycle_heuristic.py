ac6e98f37a27f4a05f7eef07c60d8bccf2c7f9a3
Marcin Zatorski <mar3211233@gmail.com>
2021-03-18 09:38:20 +0100

    Add cycle heuristic

---
+++ lab1/cycle_heuristic.py
@@ -0,0 +1,47 @@
+from typing import List, Tuple, Set
+import itertools
+from algorithm import Algorithm
+from solution import Solution
+
+class CycleHeuristic(Algorithm):
+    def run(self, instance: List[List[int]]) -> Solution:
+        n = len(instance)
+        max_len = (n + 1)//2
+        cycles: Tuple[List[int], List[int]] = self.starting_solution(n)
+        vertices: Set[int] = set(range(n))
+        vertices.remove(cycles[0][0])
+        vertices.remove(cycles[1][0])
+        total_length = 0
+
+        for i in range(n - 2):
+            best_length = 1e9
+            best_vertice = -1
+            best_cycle = -1
+            best_insert_position = -1
+
+            for v in vertices:
+                for cycle_ind, cycle in enumerate(cycles):
+                    cycle_len = len(cycle)
+                    if cycle_len >= max_len:
+                        continue
+
+                    for j in range(cycle_len):
+                        v1 = cycle[j]
+                        v2 = cycle[(j + 1) % cycle_len]
+                        length_diff = instance[v1][v] + instance[v][v2] - instance[v1][v2]
+                        if length_diff < best_length:
+                            best_length = length_diff
+                            best_vertice = v
+                            best_cycle = cycle_ind
+                            best_insert_position = j
+            
+            total_length += best_length
+            vertices.remove(best_vertice)
+            cycles[best_cycle].insert(best_insert_position + 1, best_vertice)
+
+        print(total_length)
+        return Solution(cycles, instance)
+
+
+    def starting_solution(self, n: int) -> Tuple[List[int], List[int]]:
+        return ([0], [1])
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from lab1.dummy_algorithm import DummyAlgorithm
+from lab1.cycle_heuristic import CycleHeuristic
 from read_data import read_data
 
 data = read_data('data/kroA100.tsp')
-result = DummyAlgorithm().run(data)
+result = CycleHeuristic().run(data)
 print(result)
